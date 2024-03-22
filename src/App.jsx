8058f77573f0b3d32d2f73676185766409943cdd
Hner1qwe <henriquetrachtafilho@gmail.com>
2024-03-22 16:07:18 -0400

    create a function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import footer from "./contents/footer";
 import header from "./contents/header";
-
+import main from "./main";
 import "./globals.css";                   
 
   function app(){
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {useState} from "react";
+export default function main(){
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState(0);
+  const [listatelefonica, setlistatelefonica] = usesState([]);
+  const resgistrar = (event) => {
+    event.preventDefault();
+    alert("tá lá");
+    setlistatelefonica([...listatelefonica, {
+      nomeSalvo: nome,
+      telefoneSalvo: telefone
+    }])
+  };
+  console.table(listatelefonica);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  return(
+    <main>
+      <form action="" onSubmit={registrar}>
+      <label htmlFor= "nome">
+        nome:
+        <input 
+        type="text"
+        name=""
+        id=""
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}/>
+        {name}
+      </label>
+      <br></br>
+      telefone:
+      <input 
+      type="tel"
+      name=""
+      id=""
+      value={telefone}
+      onChange={(event) => (event.target.value)}/>
+      <button>salvar bebis</button>
+      </form>
+    </main>
+
+
+  )
+}
